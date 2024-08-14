782924b5d5aa5bcb5d4b97483695d69bb25a1266
Matumbai Binale <binalematumbai@gmail.com>
2024-08-14 05:18:20 +0300

    (task 12): Log stats

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Contains the definition of `nginx_logs_stats` function"""
+from pymongo import MongoClient
+
+
+def nginx_logs_stats(log_collections):
+    """provides some stats about Nginx logs stored in MongoDB"""
+
+    print(f'{log_collections.count_documents({})} logs')
+
+    print('Methods:')
+
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in methods:
+        count = log_collections.count_documents({"method": method})
+        print('\t', f'method {method}: {count}')
+
+    count = log_collections.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+
+    print(f'{count} status check')
+
+
+def main():
+    """Dviver code for the program"""
+    client = MongoClient(host='localhost', port=27017)
+    log_collections = client.logs.nginx
+    nginx_logs_stats(log_collections)
+
+
+if __name__ == '__main__':
+    main()
