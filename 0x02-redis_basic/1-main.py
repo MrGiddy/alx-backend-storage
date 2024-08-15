03ce960d2702e4dfbaa05a92246ebd6de30936e4
Matumbai Binale <binalematumbai@gmail.com>
2024-08-16 00:05:59 +0300

    (task 2): Implement count_calls decorator

--- 0x02-redis_basic/1-main.py
+++ 0x02-redis_basic/1-main.py
@@ -3,20 +3,8 @@
 Main file
 """
 
-import redis
-
 Cache = __import__('exercise').Cache
 
-cache = Cache()
-
-data = b"hello"
-key = cache.store(data)
-print(key)
-
-local_redis = redis.Redis()
-print(local_redis.get(key))
-
-# Task 1
 cache = Cache()
 
 TEST_CASES = {
--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
-"""Writing strings to Redis"""
+"""Implements a simple cache using Redis DB"""
 import redis
 import uuid
-from typing import Union, Callable
+from typing import Union, Callable, Any, Optional
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """records the number of times a Cache instance method is called"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs) -> Any:
+        key = method.__qualname__
+        if isinstance(self._redis, redis.Redis):
+            self._redis.incr(key, 1)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache():
@@ -13,32 +25,36 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """stores cache data into a Redis DB"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+    def get(self, key: str,
+            fn: Callable = None
+            ) -> Union[str, bytes, int, float]:
         """
         retrieves data from a Redis DB,
         converts the data to the desired format and returns it
         """
         data = self._redis.get(key)
-        if data is not None and fn is not None:
-           data = fn(data)
+        if fn is not None:
+            return fn(data)
         return data
 
     def get_str(self, key: str) -> str:
         """
-        retreives data from Redis,
-        converts it to string and returns it
+        retreives data from Redis DB,
+        calls `get` to convert it to string,
+        and returns it
         """
         return self.get(key, fn=lambda d: d.decode('utf-8'))
 
     def get_int(self, key: str) -> int:
         """
-        retreives data from Redis,
+        retreives data from Redis DB,
         converts it to int and returns it
         """
         return self.get(key, fn=lambda d: int(d))
